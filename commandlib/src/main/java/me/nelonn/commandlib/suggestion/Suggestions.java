efa5c4e7a5c15c8122596558f4ae2b647897a74f
Nelonn <two.nelonn@gmail.com>
2024-02-03 21:39:47 +0300

    Suggestions Improvements

--- commandlib/src/main/java/me/nelonn/commandlib/suggestion/Suggestions.java
+++ commandlib/src/main/java/me/nelonn/commandlib/suggestion/Suggestions.java
@@ -5,22 +5,19 @@ import me.nelonn.commandlib.CommandContext;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 public class Suggestions {
     public static final List<String> EMPTY = Collections.emptyList();
     public static final List<String> SUGGEST_PLAYERS = null;
 
-    public static List<String> util(@Nullable String cursor, @NotNull List<String> values) {
-        String lower = cursor == null ? "" : cursor.toLowerCase(Locale.ENGLISH);
-        String lower2 = RussianKeyboard.ruToEn(lower);
+    public static List<String> simple(@Nullable String cursor, @NotNull Iterable<String> values) {
+        String input = cursor == null ? "" : cursor.toLowerCase(Locale.ENGLISH);
+        String input2 = RussianKeyboard.ruToEn(input);
         List<String> suggestions = new ArrayList<>();
         for (String value : values) {
-            value = value.toLowerCase(Locale.ENGLISH);
-            if (value.startsWith(lower) || value.startsWith(lower2)) {
+            String lower = value.toLowerCase(Locale.ENGLISH);
+            if (lower.startsWith(input) || lower.contains(input) || lower.startsWith(input2) || lower.contains(input2)) {
                 suggestions.add(value);
             }
         }
@@ -29,11 +26,11 @@ public class Suggestions {
 
     public static <S> List<String> children(CommandContext<S> context) {
         if (context.getArguments().length > 1) return EMPTY;
-        List<String> values = new ArrayList<>();
+        Set<String> values = new HashSet<>();
         for (Command<S> child : context.getCommand().getChildren()) {
             values.add(child.getName());
             values.addAll(child.getAliases());
         }
-        return util(context.getArguments()[0], values);
+        return simple(context.getArguments()[0], values);
     }
 }
